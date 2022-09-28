0547edf73965d3ee61e4c0fb4fd787fca79b9cb5
Ben Jordan <ben.jordan@jordandsp.com>
2022-09-28 14:32:03 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+// Important Notes:
+// 
+// - The AU2022 badge only has two buttons, which are configured as "UP" and "DOWN" controllers. Thus the movement of the spacePlane is restricted and there's no fire button. In this game, the ship just fires continually.
+// 
+// - This badge requires that the initial configuration firmware has already been loaded.
+// 
+// The firmware for this game and configuration can be found in the GitHub repository.
 controller.down.onEvent(ControllerButtonEvent.Released, function () {
     spacePlane.x += -5
 })
@@ -15,8 +22,10 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
 let projectile: Sprite = null
 let bogey: Sprite = null
 let spacePlane: Sprite = null
-pins.P23.setPull(0)
+if (control.deviceDalVersion() != "sim") {
+    pins.P23.setPull(0)
 pins.P24.setPull(0)
+}
 spacePlane = sprites.create(assets.image`spacePlane`, SpriteKind.Player)
 spacePlane.setStayInScreen(true)
 controller.moveSprite(spacePlane, 200, 200)
